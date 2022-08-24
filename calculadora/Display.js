4ef8d5ef4ab8dacdbafdbd5a766eddfd06e48682
Alexander Galleguillos <alexcris.g.s@gmail.com>
2022-08-24 13:45:37 -0400

    Se corrigio error, se cambio this.calculadora por this.calculador

--- calculadora/Display.js
+++ calculadora/Display.js
@@ -2,7 +2,7 @@ class Display {
     constructor(displayValorAnterior, displayValorActual) {
         this.displayValorActual = displayValorActual;
         this.displayValorAnterior = displayValorAnterior;
-        this.calculadora = new Calculadora();
+        this.calculador = new Calculadora();
         this.tipoOperacion = undefined;
         this.valorActual = "";
         this.valorAnterior = "";
